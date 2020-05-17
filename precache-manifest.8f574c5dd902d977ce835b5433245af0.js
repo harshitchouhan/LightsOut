08a6f9eff4d70349caf113b0e16f2d739cca2633
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-17 18:57:22 +0530

    Updates

---
+++ precache-manifest.8f574c5dd902d977ce835b5433245af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ba17537c185bfe2f483",
+    "url": "/LightsOut/static/css/main.8fa8a5a9.chunk.css"
+  },
+  {
+    "revision": "0ba17537c185bfe2f483",
+    "url": "/LightsOut/static/js/main.0ba17537.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/LightsOut/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "135e11432d80503c488c",
+    "url": "/LightsOut/static/js/runtime~main.135e1143.js"
+  },
+  {
+    "revision": "029469b5a9bc8e066f69c6ed1ec3f239",
+    "url": "/LightsOut/index.html"
+  }
+];
